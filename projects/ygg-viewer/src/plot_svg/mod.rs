f10b6df657e2df91d56e391bd9720d8f1d7f1ceb
Aster <aster@vers.site>
2023-10-20 22:03:47 +0800

    Fix width problem

--- projects/ygg-viewer/src/plot_svg/mod.rs
+++ projects/ygg-viewer/src/plot_svg/mod.rs
@@ -1,7 +1,13 @@
-use std::collections::HashMap;
+use std::cmp::max;
+
+use shape_svg::ToSVG;
+use svg::{
+    node::element::{Text, SVG},
+    Document,
+};
 use yggdrasil_rt::{TokenPair, TokenTree, YggdrasilRule};
 
-use tree_layout::{layout, layout_position, NodeInfo, Point, Rectangle, TreeData, TreeNode};
+use tree_layout::{layout, NodeInfo, Rectangle, TreeData, TreeNode};
 
 #[derive(Debug)]
 pub struct SvgTree<'i, R>
@@ -31,22 +37,29 @@ where
         let v: Vec<_> = node.into_inner().filter(|s| !s.get_rule().is_ignore()).collect();
         v
     }
-
     fn dimensions(&self, node: TokenPair<'i, R>) -> Rectangle {
-        let chars = format!("{:?}", node.get_rule()).len() as f64;
-        Rectangle::from_origin(chars * 16.0, 40.0)
+        let chars = width_hint(node);
+        Rectangle::from_origin(chars * 16.0, 32.0)
     }
     fn border(&self, _: TokenPair<'i, R>) -> Rectangle {
-        Rectangle::from_origin(10.0, 20.0)
+        Rectangle::from_origin(20.0, 20.0)
     }
 }
 
+fn width_hint<R>(node: TokenPair<R>) -> f64
+where
+    R: YggdrasilRule,
+{
+    let text = if node.has_child() { format!("{:?}", node.get_rule()) } else { node.get_string() };
+    max(text.len(), 3) as f64
+}
+
 impl SvgPlotter {
-    pub fn draw<'i, R>(&self, tree: TokenTree<'i, R>) -> SvgTree<'i, R>
+    pub fn draw<R>(&self, tree: TokenTree<R>) -> SVG
     where
         R: YggdrasilRule,
     {
-        SvgTree { cst: tree }
+        SvgTree { cst: tree }.as_svg()
     }
 }
 
@@ -54,9 +67,26 @@ impl<'i, R> SvgTree<'i, R>
 where
     R: YggdrasilRule,
 {
-    pub fn position(&self) -> Vec<TreeNode<TreeData<TokenPair<'i, R>>>> {
+    fn as_svg(&self) -> SVG {
+        let mut document = Document::new().set("viewBox", (0, 0, 3000, 3000));
         let root = self.cst.clone().into_iter().next().unwrap();
         let layout = layout(self, root);
-        layout.into_iter().collect()
+        for node in layout {
+            let area = node.data.boundary().to_svg().set("fill", "none").set("stroke", "black").set("stroke-width", 0.1);
+            document = document.add(area);
+            let pair = node.data.key.clone();
+            let mut text = Text::new()
+                .set("x", node.data.center().x - width_hint(pair.clone()) * 4.0)
+                .set("y", node.data.boundary().min.y + 24.0);
+            if pair.has_child() {
+                text = text.add(svg::node::Text::new(format!("{:?}", pair.get_rule())));
+            }
+            else {
+                text = text.add(svg::node::Text::new(format!("{}", pair.get_string())));
+            }
+            document = document.add(text);
+        }
+
+        document
     }
 }
